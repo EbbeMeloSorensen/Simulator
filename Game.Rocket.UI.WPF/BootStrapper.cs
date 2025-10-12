43d26e6eeac666a8d2edf97243b42f1a2774ccbf
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-10-12 11:42:02 +0200

    -

---
+++ Game.Rocket.UI.WPF/BootStrapper.cs
@@ -0,0 +1,16 @@
+﻿using StructureMap;
+using Game.Rocket.ViewModel;
+
+namespace Game.Rocket.UI.WPF
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                return Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+            }
+        }
+    }
+}
---
+++ Game.Rocket.UI.WPF/CustomizedShapeTemplateSelector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Craft.ViewModels.Geometry2D.ScrollFree;
+using Game.Rocket.ViewModel.ShapeViewModels;
+
+namespace Game.Rocket.UI.WPF
+{
+    public class CustomizedShapeTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(
+            object item,
+            DependencyObject container)
+        {
+            var element = container as FrameworkElement;
+
+            switch (item)
+            {
+                case RocketViewModel _:
+                {
+                    return element.FindResource("Rocket") as DataTemplate;
+                }
+                case MeteorViewModel _:
+                {
+                    return element.FindResource("Meteor") as DataTemplate;
+                }
+                case FragmentViewModel _:
+                {
+                        return element.FindResource("Fragment") as DataTemplate;
+                }
+                case ProjectileViewModel _:
+                {
+                    return element.FindResource("Projectile") as DataTemplate;
+                }
+                case RectangleViewModel _:
+                {
+                    return element.FindResource("Wall") as DataTemplate;
+                }
+                default:
+                {
+                    throw new ArgumentException("item doesn't correspond to any DataTemplate");
+                }
+            }
+        }
+    }
+}
