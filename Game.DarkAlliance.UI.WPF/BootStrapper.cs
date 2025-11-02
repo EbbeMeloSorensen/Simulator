f134a39c36935c881f997e01d5f9793b226472be
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2025-11-02 12:29:29 +0100

    Satte Dark Alliance i søen

---
+++ Game.DarkAlliance.UI.WPF/BootStrapper.cs
@@ -0,0 +1,16 @@
+﻿using StructureMap;
+using Game.DarkAlliance.ViewModel;
+
+namespace Game.DarkAlliance.UI.WPF
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                return Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+            }
+        }
+    }
+}
---
+++ Game.DarkAlliance.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+
+using Craft.Logging;
+using Craft.ViewModels.Geometry2D.ScrollFree;
+using GalaSoft.MvvmLight;
+
+namespace Game.DarkAlliance.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _greeting;
+        private ILogger _logger;
+
+        public string Greeting
+        {
+            get => _greeting;
+            set
+            {
+                if (value == _greeting) return;
+
+                _greeting = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public GeometryEditorViewModel GeometryEditorViewModel { get; }
+
+        public MainWindowViewModel(
+            ILogger logger)
+        {
+            _greeting = "Hej fra Dark Alliance MainWindowViewModel";
+
+            _logger = logger;
+            _logger = null; // Disable logging (it should only be used for debugging purposes)
+
+        }
+    }
+}
