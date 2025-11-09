1416e9fdaa040f7ead8eabb0884128417fda4d8b
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2025-11-09 13:36:54 +0100

    -

---
+++ Game.Simple.UI.WPF/BootStrapper.cs
@@ -0,0 +1,16 @@
+﻿using Game.Simple.ViewModel;
+using StructureMap;
+
+namespace Game.Simple.UI.WPF
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                return Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+            }
+        }
+    }
+}
